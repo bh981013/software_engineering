d19f7a2e3828e074e7eb1796abeaf8b08883fbdf
Librarian423 <81158058+Librarian423@users.noreply.github.com>
2021-12-07 22:20:26 +0900

    push

---
+++ app/src/main/java/org/techtown/se_project05/ManagerSelectAttendance.java
@@ -0,0 +1,18 @@
+package org.techtown.se_project05;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagerSelectAttendance extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.manager_select_attendance);
+    }
+}
+
+
+
+

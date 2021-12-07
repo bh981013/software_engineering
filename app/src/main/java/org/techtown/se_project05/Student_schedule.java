766303327eaed4caafba55f158a1a2b489d83721
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2021-12-07 19:28:59 +0900

    se

---
+++ app/src/main/java/org/techtown/se_project05/Student_schedule.java
@@ -0,0 +1,25 @@
+package org.techtown.se_project05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class Student_schedule extends AppCompatActivity {
+
+    private Spinner sch_year,sch_month,sch_day;
+    private Button btn_schedule_check,btn_schedule_undo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_schedule);
+
+        sch_year = (Spinner)findViewById(R.id.sch_year);
+        sch_month = (Spinner)findViewById(R.id.sch_month);
+        sch_day = (Spinner)findViewById(R.id.sch_day);
+
+
+    }
+}

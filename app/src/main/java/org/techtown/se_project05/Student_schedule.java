97e23505ea50e3e6bf96d31a945bdde23c46cfd2
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2021-12-07 20:03:37 +0900

    se

--- app/src/main/java/org/techtown/se_project05/Student_schedule.java
+++ app/src/main/java/org/techtown/se_project05/Student_schedule.java
@@ -2,14 +2,28 @@ package org.techtown.se_project05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class Student_schedule extends AppCompatActivity {
 
     private Spinner sch_year,sch_month,sch_day;
-    private Button btn_schedule_add,btn_schedule_back,btn_schedule_look;
+    private Button btn_add,btn_back,btn_look;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,14 @@ public class Student_schedule extends AppCompatActivity {
         sch_year = (Spinner)findViewById(R.id.sch_year);
         sch_month = (Spinner)findViewById(R.id.sch_month);
         sch_day = (Spinner)findViewById(R.id.sch_day);
+
+        btn_add = findViewById(R.id.btn_schedule_add);
+
+        btn_look = findViewById(R.id.btn_schedule_look);
+
+        btn_back = findViewById(R.id.btn_schedule_back);
+
+
 
 
     }
